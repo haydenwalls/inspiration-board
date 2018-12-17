eb956f50eebb25ea97ae5c3a7dc1c59de2b61a6b
Hayden Walls <hwilliams.deva@gmail.com>
2018-12-16 18:50:08 -0800

    add delCard function to Board and pass as callback prop to each card so cards can be deleted

--- src/components/Board.js
+++ src/components/Board.js
@@ -16,10 +16,71 @@ class Board extends Component {
     };
   }
 
+  componentDidMount() {
+    axios.get('https://inspiration-board.herokuapp.com/boards/ultra_princess/cards')
+      .then((res) => {
+        this.setState({ cards: res.data })
+      })
+      .catch((error) => {
+        alert('hm ur cards didnt load maybe u should refresh lol');
+      });
+  }
+
+  delCard = (id) => {
+    const url = 'https://inspiration-board.herokuapp.com/cards/' + id.toString();
+
+    axios.delete(url)
+      .then(() => {
+        let index;
+        let newState = this.state;
+
+        newState.cards.forEach((card, i) => {
+          if (card.card.id === id) {
+            index = i;
+          }
+        });
+
+        newState.cards.splice(index, 1);
+
+        this.setState(newState);
+      })
+      .catch((e) => {
+        alert('sry could not delete ?? idk tbh..');
+        console.log(e);
+      });
+  }
+
+  addCard = (newCard) => {
+    const url = `https://inspiration-board.herokuapp.com/boards/ultra_princess/cards?text=${newCard.text}&emoji=${newCard.emoji}`;
+
+    axios.post(url)
+      .then((res) => {
+        let newState = this.state;
+        newState.cards.push(res.data);
+
+        this.setState(newState);
+      })
+      .catch((e) => {
+        alert('sry could not add card ?? idk tbh..');
+        console.log(e);
+      });
+  }
+
   render() {
+    const cards = this.state.cards.map((e, i) => {
+      return (
+        <Card key={ e.card.id }
+          id={ e.card.id }
+          text={ e.card.text }
+          emoji={ e.card.emoji }
+          delCB={ this.delCard } />
+      )
+    });
+
     return (
-      <div>
-        Board
+      <div className="board">
+        { cards }
+        <NewCardForm addCB={ this.addCard } />
       </div>
     )
   }
--- src/components/Card.js
+++ src/components/Card.js
@@ -8,7 +8,11 @@ class Card extends Component {
   render() {
     return (
       <div className="card">
-        Card
+        <div className="card__content">
+          <p className="card__content-text">{ this.props.text }</p>
+          <p className="card__content-emoji">{ this.props.emoji ? emoji.getUnicode(this.props.emoji) : "" }</p>
+          <button onClick={() => {this.props.delCB(this.props.id)} } >del mee</button>
+        </div>
       </div>
     )
   }
